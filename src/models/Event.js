2047c543b39b3776925e3b0b2b3c0c91ce6907cd
Lazarov Robert <robertlazarov13@gmail.com>
2024-01-30 20:56:27 +0200

    started db

---
+++ src/models/Event.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const EventSchema = new mongoose.Schema({
+    title: {type: String, required: true,unique: [true, 'An event with this name already exists!'] },
+    date: {type: Date, required: true},
+    location: {type: String, required: true},
+    description: {type: String, required: true},
+    tags: {type: String,},
+    invites: [String],
+    participants: [String],
+});
+
+const Event = mongoose.model('Event', EventSchema);
+
+module.exports = Event;
