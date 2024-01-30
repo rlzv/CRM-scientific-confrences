97b35caeff316b1c7380b818ab4a29e44f5dee3f
Lazarov Robert <robertlazarov13@gmail.com>
2024-01-30 21:10:38 +0200

    user schema

---
+++ backend/src/models/User.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    username: {type: String, required: true, unique: true},
+    password: { type: String, required: true},
+    role: {type: String, enum: ['organizer', 'attendee'],},
+});
+
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User
